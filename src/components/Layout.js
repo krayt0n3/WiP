123777ce9819b3b3e7bbf94039542f143266cda2
r8yt <hendersonkevin64@gmail.com>
2020-01-22 09:05:19 -0600

    bootstrap working now

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -14,6 +14,7 @@ const TemplateWrapper = ({ children }) => {
         <html lang="en" />
         <title>{title}</title>
         <meta name="description" content={description} />
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"></link>
 
         <link
           rel="wifi-icon"
